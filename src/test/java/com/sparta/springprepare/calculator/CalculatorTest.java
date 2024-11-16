9c7d1c7782be95180deb1be02115e46342401043
parkJJae <wgpark0830@naver.com>
2024-11-16 21:04:29 +0900

    Test 코드

---
+++ src/test/java/com/sparta/springprepare/calculator/CalculatorTest.java
@@ -0,0 +1,28 @@
+package com.sparta.springprepare.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// Test 코드는 Main이 없음 Junit -> Test 실행 환경을 따로 갖고 있음.
+class CalculatorTest {
+    @Test
+    @DisplayName("더하기 테스트")
+    void test1() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(8, "+", 2);
+        System.out.println("result = " + result);
+
+        Assertions.assertEquals(10, result);
+    }
+
+    @Test
+    @DisplayName("나누기 테스트")
+    void test2() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(8, "/", 2);
+        System.out.println("result = " + result);
+
+        Assertions.assertEquals(4, result);
+    }
+}
